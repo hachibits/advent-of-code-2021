1a2a21c39f02d0ac6cb0f4a636c2b1fefbb338f1
hachibits <ubuntu@DESKTOP-VTP3E3P.localdomain>
2021-12-30 18:59:52 +1100

    fixed day04

--- day04/04.py
+++ day04/04.py
@@ -1,26 +1,22 @@
-nums = [int(x) for x in open("input").readline().strip().split(",")]
-boards = [
-    [[int(c) for c in r.split()] for r in mat.splitlines()]
-    for mat in open("input").read().strip().split("\n")[1:]
-]
-
 def solve():
+    nums = [int(x) for x in open("input").readline().strip().split(",")]
+    boards = [
+        [[int(c) for c in r.split()] for r in mat.splitlines()]
+        for mat in open("input").read().strip().split("\n\n")[1:]
+    ]
     for n in nums: 
         for b in boards:
             for i, row in enumerate(b):
                 for j, cell in enumerate(row):
                     if cell == n:
-                        b[i][j] == "X"
-            
+                        b[i][j] = "X"
             for row in b:
-                #print(sum(cell == "X" for cell in row))
-                if sum(cell == "X" for cell in row) == 5:
-                    score = sum(cell != "X" for cell in row for row in b)
+                if row.count("X") == 5: 
+                    score = sum(cell for r in b for cell in r if cell != "X")
                     return score * n
-
             for col in zip(*b):
-                if sum(cell == "X" for cell in col) == 5:
-                    score = sum(cell != "X" for cell in row for row in b)
+                if col.count("X") == 5:
+                    score = sum(cell for r in b for cell in r if cell != "X")
                     return score * n
 
 if __name__ == "__main__":
